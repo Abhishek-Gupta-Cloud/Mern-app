2fe4cb229ba1817b7fed605901fa6525b3480a59
Abhishek Gupta <shivagupta9765@gmail.com>
2026-06-10 15:20:11 +0530

    mongodb

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -469,3 +469,37 @@ resource "aws_security_group" "alb" {
     Name = "${var.cluster_name}-alb-sg"
   })
 }
+data "aws_iam_policy_document" "ebs_csi_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.cluster.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.main.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
+}
+resource "aws_iam_role" "ebs_csi" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_assume_role.json
+}
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
